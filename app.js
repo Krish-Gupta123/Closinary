9a2379a72db6882a77bde8f3d45fc8efffbda555
Krish-Gupta123 <krishrgupta5@gmail.com>
2026-02-17 17:18:24 +0530

    Listing Models

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const Listings = require('./models/listing.js')
 
 const MONGO_URL = 'mongodb://127.0.0.1:27017/closinary';
 
@@ -10,12 +11,26 @@ main()
 
 async function main() {
     await mongoose.connect(MONGO_URL);
-
 }
 
 
 app.get('/', (req, res) => {
     res.send("Hi i am root")
+})
+
+app.get('/testListing', async (req, res) => {
+    const sampleListing = new Listings({
+        title: "test listing",
+        description: "my beach side villa",
+        price: 1000,
+        location: "beach",
+        country: "italy"
+    })
+
+    await sampleListing.save();
+    console.log("sample was saved");
+    res.send("successfully tested");
+
 })
 
 app.listen(8080, () => {
---
+++ models/listing.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: String,
+    image: {
+        type: String,
+        default: "https://unsplash.com/photos/modern-skyscraper-with-glass-facade-against-cloudy-sky-lEsfGOaF4nY",
+        set(v) {
+            v === ""
+                ? "https://unsplash.com/photos/a-pool-of-water-surrounded-by-rocks-and-trees-zo_udYMcaVc"
+                : v
+        }
+    },
+    price: Number,
+    location: String,
+    country: String,
+})
+
+const Listings = mongoose.model('Listings', listingSchema);
+module.exports = Listings;
