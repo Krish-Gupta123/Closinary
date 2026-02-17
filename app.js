cff129bfc18d60a4a76068b42f5d709f5e1dac52
Krish-Gupta123 <krishrgupta5@gmail.com>
2026-02-17 20:27:25 +0530

    Show route

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const Listings = require('./models/listing.js');
+const Listing = require('./models/listing.js');
 const path = require('path');
 
 const MONGO_URL = 'mongodb://127.0.0.1:27017/closinary';
@@ -16,16 +16,23 @@ async function main() {
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.send("Hi i am root")
 })
 
-
 // index route 
 app.get('/listings', async (req, res) => {
-    const allListing = await Listings.find({});
+    const allListing = await Listing.find({});
     res.render('listings/index.ejs', { allListing });
+})
+
+// show route
+app.get('/listings/:id', async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render('listings/show.ejs', { listing });
 })
 
 app.listen(8080, () => {
